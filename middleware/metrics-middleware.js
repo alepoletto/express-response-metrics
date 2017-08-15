bce2c58e302914c5cca1c2280a525a2471359e08
Alexandre Poletto <alexandrepoletto@gmail.com>
2017-08-15 12:58:15 -0700

    fixed an issue

--- middleware/metrics-middleware.js
+++ middleware/metrics-middleware.js
@@ -1,24 +1,29 @@
-const Chronometer = require('./Chronometer');
+const Chronometer = require('./chronometer');
 const Collector = require('./metrics-collector');
-const MetricsServer = require('./metrics-server')
+const MetricsServer = require('./metrics-server');
 
-const metricMiddleware = (options) => {
+const metricMiddleware = options => {
   const server = new MetricsServer();
   const collector = new Collector();
-  let port = options && options.port || 8190;
+  let port = (options && options.port) || 8190;
   server.init(port, collector);
 
   return (req, res, next) => {
-      let chronometer = new Chronometer();
-      chronometer.start();
-      let end = res.end;
-      res.end = function ()  {
-        let responseTime = chronometer.stop();
-        end.apply(res, arguments);
-        collector.collect(req.originalUrl, req.method, res.statusCode, responseTime);
-      };
-      next();
-    }
-}
+    let chronometer = new Chronometer();
+    chronometer.start();
+    let end = res.end;
+    res.end = function() {
+      let responseTime = chronometer.stop();
+      end.apply(res, arguments);
+      collector.collect(
+        req.originalUrl,
+        req.method,
+        res.statusCode,
+        responseTime
+      );
+    };
+    next();
+  };
+};
 
 module.exports = metricMiddleware;
